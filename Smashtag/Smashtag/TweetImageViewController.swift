42c549716bae9196707cee13fa2275522378e7a3
Eelco <eelcowagenaar@hotmail.com>
2015-05-08 12:50:33 +0200

    copied code from SingleTweetView to show the image

--- Smashtag/Smashtag/TweetImageViewController.swift
+++ Smashtag/Smashtag/TweetImageViewController.swift
@@ -9,9 +9,32 @@
 import UIKit
 
 class TweetImageViewController: UIViewController {
-    var imageUrl: NSURL?
+    var imageUrl: NSURL? {
+        didSet {
+            updateUI()
+        }
+    }
     
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         println("\(imageUrl)")
+    }
+    
+    func updateUI() {
+        imageView?.image = nil
+        if let url = imageUrl {
+            dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_USER_INITIATED.value), 0)) {
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue()) {
+                    if url == self.imageUrl {
+                        if imageData != nil {
+                            self.imageView?.image = UIImage(data: imageData!)
+                        } else {
+                            self.imageView?.image = nil
+                        }
+                    }
+                }
+            }
+        }
     }
 }
