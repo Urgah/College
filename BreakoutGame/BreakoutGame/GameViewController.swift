c602b1dbb29beb04e94be4f4c0b7bebcb80b68c8
Eelco <eelcowagenaar@hotmail.com>
2015-05-19 13:17:36 +0200

    Added paddle

--- BreakoutGame/BreakoutGame/GameViewController.swift
+++ BreakoutGame/BreakoutGame/GameViewController.swift
@@ -23,6 +23,14 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         animator.addBehavior(breakout)
         gameView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "pushBall:"))
+        
+        gameView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "panPaddle:"))
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: "swipePaddleLeft:")
+        swipeLeft.direction = .Left
+        gameView.addGestureRecognizer(swipeLeft)
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "swipePaddleRight:")
+        swipeRight.direction = .Right
+        gameView.addGestureRecognizer(swipeRight)
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,11 +52,16 @@ class GameViewController: UIViewController {
                 animator.updateItemUsingCurrentState(ball)
             }
         }
+        
+        if !CGRectContainsRect(gameView.bounds, paddle.frame) {
+            resetPaddle()
+        }
     }
     
     // ball
     func placeBall(ball: UIView) {
-        var center = CGPoint(0,0)
+        var center = paddle.center
+        center.y -= self.PaddleSize.height / 2 + self.PaddleSize.height
         ball.center = center
     }
     
@@ -73,5 +86,63 @@ class GameViewController: UIViewController {
         ball.layer.shadowOpacity = 0.5
         return ball
     }
+
+    // paddle
+    lazy var paddle: UIView = {
+        let paddle = UIView(frame: CGRect(origin: CGPoint(x: -1, y: -1), size: self.PaddleSize))
+        paddle.backgroundColor = self.PaddleColor
+        paddle.layer.cornerRadius = self.PaddleCornerRadius
+        paddle.layer.borderColor = UIColor.blackColor().CGColor
+        paddle.layer.borderWidth = 2.0
+        paddle.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        paddle.layer.shadowOpacity = 0.5
+        
+        self.gameView.addSubview(paddle)
+        return paddle
+    }()
+    
+    func resetPaddle() {
+        //We dont want the paddle to be in the buttom so we place it 80 points up
+        paddle.center = CGPoint(x: gameView.bounds.midX, y: gameView.bounds.maxY - paddle.bounds.height - 80)
+        addPaddleBarrier()
+    }
+    
+    func addPaddleBarrier() {
+        breakout.addBarrier(UIBezierPath(roundedRect: paddle.frame, cornerRadius: self.PaddleCornerRadius), named: "Paddle")
+    }
+    
+    func panPaddle(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            placePaddle(gesture.translationInView(gameView))
+            gesture.setTranslation(CGPointZero, inView: gameView)
+        default: break
+        }
+    }
+    
+    func swipePaddleLeft(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended:
+            placePaddle(CGPoint(x: -gameView.bounds.maxX, y: 0.0))
+        default: break
+        }
+    }
+    
+    func swipePaddleRight(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+            case .Ended:
+                placePaddle(CGPoint(x: gameView.bounds.maxX, y: 0.0))
+            default: break
+        }
+    }
+    
+    func placePaddle(translation: CGPoint) {
+        var origin = paddle.frame.origin
+        origin.x = max(min(origin.x + translation.x, gameView.bounds.maxX - self.PaddleSize.width), 0.0)
+        paddle.frame.origin = origin
+        addPaddleBarrier()
+    }
+
 }
 
